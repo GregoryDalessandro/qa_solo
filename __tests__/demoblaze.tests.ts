ed8990f06a05939b88c51e3bc64ee119dbf23e42
GregoryDalessandro <gdalessandro418@gmail.com>
2021-03-31 22:40:19 -0400

    pass add to cart test

--- __tests__/demoblaze.tests.ts
+++ __tests__/demoblaze.tests.ts
@@ -61,12 +61,51 @@ const driver: WebDriver = new Builder()
       await demoblaze.click(By.xpath("//a[@href='prod.html?idp_=1']"));
       expect(await driver.getCurrentUrl()).toEqual("https://www.demoblaze.com/prod.html?idp_=1");
     });
-    test("user can filter categories", async() => {});
+    // test("user can filter categories", async() => {
+    //   let phones = ["Samsung galaxy s6", "Nokia lumia 1520", "Nexus 6", "Samsung galaxy s7", "Iphone 6 32gb", "Sony xperia z5", "HTC One M9"]
+    //   // let laptops = ["Sony vaio i5", "Sony vaio i7", "MacBook air", "Dell i7 8gb", "2017 Dell 15.6 Inch", "MacBook Pro"];
+    //   // let monitors = ["Apple monitor 24", "ASUS Full HD"];
+    //   // filter phones
+    //   await demoblaze.click(By.xpath("//a[contains(text(),'Phones')]"));
+    //   // expect there to only be phones
+
+    //   let hardCodedFilteredBodyElements = [
+    //     await demoblaze.getText(By.xpath("//a[contains(text(),'Samsung galaxy s6')]")),
+    //     await demoblaze.getText(By.xpath("//a[contains(text(),'Nokia lumia 1520')]")),
+    //     await demoblaze.getText(By.xpath("//a[contains(text(),'Nexus 6')]")),
+    //     await demoblaze.getText(By.xpath("//a[contains(text(),'Samsung galaxy s7')]")),
+    //     await demoblaze.getText(By.xpath("//a[contains(text(),'Iphone 6 32gb')]")),
+    //     await demoblaze.getText(By.xpath("//a[contains(text(),'Sony xperia z5)]")),
+    //     await demoblaze.getText(By.xpath("//a[contains(text(),'HTC One M9)]"))
+    //   ];
+    //   for (let i=0, j=0; i < phones.length, j < hardCodedFilteredBodyElements.length; i++, j++) {
+    //     // console.log(hardCodedFilteredBodyElements[j]);
+    //     expect(phones[i]).toContain(hardCodedFilteredBodyElements[j])
+    //   }
+
+      // expect body to contain each of the products in phones
+
+      // click laptops
+      // expect there to be only laptops
+      // click monitors
+      // expect there to be only monitors
+    // });
     test("user can select previous and next buttons", async() => {
       //expect the main page to show the "first product page" (however you can select previous)
     });
     // product and cart tests
-    test("user can add a product to the cart", async() => {});
+    test("user can add a product to the cart", async() => {
+      // navigate to a product page, add to cart, then navigate to cart
+      await demoblazeProduct.click(By.xpath("//a[contains(text(), 'Samsung galaxy s6')]"));
+      await demoblazeProduct.addToCart();
+      // wait for the alert to show up
+      await driver.wait(until.alertIsPresent());
+      let alert = await driver.switchTo().alert();
+      await alert.accept();
+      await demoblazeProduct.click(demoblazeProduct.cart);
+      //expect the product to be in the cart
+      expect(await demoblazeProduct.getElement(By.css(".success"))).toBeTruthy();
+    });
     test("user can add multiple products to the cart", async() => {});
     test("the products remain in the cart after navigating away ", async() => {});
     test("user can remove a product from the cart", async() => {});
